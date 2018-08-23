e890ec795401f117908189be879d4ae2156c2301
Jed Peek <jedpeek@yahoo.com>
2018-08-23 18:36:55 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,9 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
   accepts_nested_attributes_for :categories
 
+  def categories_attributes=(categories_hashes)
+    categories_hashes.each do |i, attributes|
+      self.post_categories.build{category: category}
+
 
 end
